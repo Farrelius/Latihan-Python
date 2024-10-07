f2b23637dfd7e4ea7ea8aaae71cd1c669897ff32
= <=>
2024-10-07 17:13:40 +0700

    Second commit

--- kalkulator.py
+++ kalkulator.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print(f'''{55*"="}
 KALKULATOR 2 BILANGAN OPERASI ARITMATIKA DARI PYTHON BY FARREL
 {55*"="}''')
@@ -24,31 +23,4 @@ elif operasi_bilangan == "x":
 elif operasi_bilangan == ":":
     print(f"{angka_1} : {angka_2} = {angka_1/angka_2}")
 else:
-=======
-print(f'''{55*"="}
-KALKULATOR 2 BILANGAN OPERASI ARITMATIKA DARI PYTHON BY FARREL
-{55*"="}''')
-
-print(f'''{45*"="}
-    OPERASI ARITMATIKA DUA BILANGAN
-    {45*"="}''')
-
-# AMBIL INPUT
-
-angka_1 = float(input("Masukkan Angka ke 1 : "))
-operasi_bilangan = str(input("Masukkan operasi bilangan (+, -, x, : ): "))
-angka_2 = float(input("Masukkan Angka ke 2 : "))
-
-# ALGORITMA DAN HASIL
-
-if operasi_bilangan == "+":
-    print(f"{angka_1} + {angka_2} = {angka_1+angka_2}")
-elif operasi_bilangan == "-":
-    print(f"{angka_1} - {angka_2} = {angka_1-angka_2}")
-elif operasi_bilangan == "x":
-    print(f"{angka_1} x {angka_2} = {angka_1*angka_2}")
-elif operasi_bilangan == ":":
-    print(f"{angka_1} : {angka_2} = {angka_1/angka_2}")
-else:
->>>>>>> 4b56fd6a9cadec6fb89a2dd9343ea9458f3d7821
     print("Ada yang salah, coba lagi")
