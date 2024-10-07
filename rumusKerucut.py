37e235889cc0c5cd2562189171fd69a4b065d8bc
= <=>
2024-10-07 19:50:52 +0700

    first commit

---
+++ rumusKerucut.py
@@ -0,0 +1,18 @@
+# PROGRAM FORMULA KERUCUT
+# 07/10/2024 19:37
+
+# MENGAMBIL DATA DARI INPUT USER
+
+r = int(input("Masukkan jari jari kerucut : "))
+s = int(input("Masukkan sisi kerucut : "))
+t = int(input("Masukkan tinggi kerucut : "))
+
+# ALGORITMA
+
+luasK = 3.14*r(r+s)
+volumeK = 1/3*3.14*r*t
+
+# HASIL/OUTPUT
+
+print(f'''LUAS KERUCUT : {luasK}
+VOLUME KERUCUT : {volumeK}''')
