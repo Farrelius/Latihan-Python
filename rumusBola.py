c7079ef5da5abf81d6f7e917a194b15415f61232
= <=>
2024-10-07 19:34:56 +0700

    first commit

---
+++ rumusBola.py
@@ -0,0 +1,16 @@
+# PROGRAM FORMULA BOLA BY FARREL
+# 07/10/2024 19:26
+
+# MENGAMBIL DATA DARI INPUT USER
+
+r = int(input("Masukkan jari jari bola : "))
+
+# ALGORITMA
+
+luasB = 4*3.14*r**2
+volumeB = 4/3*3.14*r**3
+
+# HASIL/OUTPUT
+
+print(f'''LUAS BOLA : {luasB}
+VOLUME BOLA : {volumeB}''')
