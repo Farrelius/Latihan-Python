80c3c357ff22669c0ba9b1801d92d5b62d5f04f3
= <=>
2024-10-08 08:43:53 +0700

    second commit

--- rumusLimasSegitiga.py
+++ rumusLimasSegitiga.py
@@ -1,18 +1,17 @@
 # PROGRAM FORMULA LIMAS SEGITIGA BY FARREL
 # 08/10/2024 07:02
+def limas_segitiga():
+    # MENGAMBIL DATA DARI INPUT USER
 
-# MENGAMBIL DATA DARI INPUT USER
-
-LA = int(input("Masukkan Luas alasnya : "))
-LST = int(input("Masukkan Luas Sisi Tegaknya : "))
-T = int(input("Masukkan Tingginya : "))
+    LA = int(input("Masukkan Luas Alas : "))
+    LST = int(input("Masukkan Luas Sisi Tegak : "))
+    T = int(input("Masukkan Tinggi : "))
 
 # ALGORITMA
-
-LP= LA + (3*LST)
-V = 1/3 * LA * T
-
-# OUTPUT/HASIL
-
-print(f'''LUAS PERMUKAAN LIMAS SEGITIGA : {LP}
+    
+    LP= LA + (3*LST)
+    V = 1/3 * LA * T
+    print(f'''LUAS PERMUKAAN LIMAS SEGITIGA : {LP}
 VOLUME LIMAS SEGITIGA : {V}''')
+
+limas_segitiga()
