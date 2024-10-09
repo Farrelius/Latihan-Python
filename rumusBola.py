c8790091f6e1b82677665acdcae6c6b5bb1ec6f0
= <=>
2024-10-09 20:48:12 +0700

    second commit

--- rumusBola.py
+++ rumusBola.py
@@ -3,14 +3,17 @@
 
 # MENGAMBIL DATA DARI INPUT USER
 
-r = int(input("Masukkan jari jari bola : "))
+def bola():
+    r = int(input("Masukkan jari jari bola : "))
 
 # ALGORITMA
 
-luasB = 4*3.14*r**2
-volumeB = 4/3*3.14*r**3
+    luasB = lambda r : 4*3.14*r**2
+    volumeB = lambda r : 4/3*3.14*r**3
 
 # HASIL/OUTPUT
 
-print(f'''LUAS BOLA : {luasB}
-VOLUME BOLA : {volumeB}''')
+    print(f'''LUAS BOLA : {luasB(r)}
+VOLUME BOLA : {volumeB(r)}''')
+    
+bola()
