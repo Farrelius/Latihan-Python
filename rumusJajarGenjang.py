ed7248e59ab2156c59cba44d72e46fbc865fd9e0
= <=>
2024-10-09 20:54:50 +0700

    second commit

--- rumusJajarGenjang.py
+++ rumusJajarGenjang.py
@@ -1,18 +1,20 @@
 # PROGRAM FORMULA JAJAR GENJANG BY FARREL
 # 07/10/2024 14:32
 
+def jajarGenjang():
 # MENGAMBIL DATA DARI INPUT USER
-
-a = int(input("Masukkan alas jajar genjang : "))
-t = int(input("Masukkan tinggi jajar genjang : "))
-b = int(input("Masukkan sudut jajar genjang : "))
+    a = int(input("Masukkan alas jajar genjang : "))
+    t = int(input("Masukkan tinggi jajar genjang : "))
+    b = int(input("Masukkan sudut jajar genjang : "))
 
 # ALGORITMA
 
-luasJG = a*t
-kelilingJG = 2*a+b
+    luasJG = lambda a,t : a*t
+    kelilingJG = lambda a,b : 2*a+b
 
 # OUTPUT/HASIL
 
-print(f'''LUAS JAJAR GENJANG : {luasJG}
-KELILING JAJAR GENJANG : {kelilingJG}''')
+    print(f'''LUAS JAJAR GENJANG : {luasJG(a,t)}
+    KELILING JAJAR GENJANG : {kelilingJG(a,b)}''')
+
+jajarGenjang()
