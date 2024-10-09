65dd3d708aa9edb3f5f9e2aa394e6b8aa5258bc6
= <=>
2024-10-09 21:00:15 +0700

    second commit

--- rumusLingkaran.py
+++ rumusLingkaran.py
@@ -1,16 +1,20 @@
 # FORMULA LINGKARAN BY FARREL
 # 05/10/2024 16:01
 
+def lingkaran():
+
 # MENGAMBIL INPUT DATA DARI USER
 
-r = int(input("Masukkan jari jari lingkaran : "))
+    r = int(input("Masukkan jari jari lingkaran : "))
 
 # ALGORIMTA
 
-luasL = 3.14 * r**2
-kelilingL = 2*3.14*r
+    luasL = lambda r : 3.14 * r**2
+    kelilingL = lambda r : 2*3.14*r
 
 # OUTPUT/HASIL
 
-print(f'''LUAS LINGKARAN : {luasL}cm
-KELILING LINGKARAN : {round(kelilingL)}cm''')
+    print(f'''LUAS LINGKARAN : {luasL(r)}cm
+KELILING LINGKARAN : {round(kelilingL(r))}cm''')
+    
+lingkaran()
