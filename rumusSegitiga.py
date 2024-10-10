d8bfe316ea4d51017142e2ca2fe389e847e7b9bb
= <=>
2024-10-10 07:54:32 +0700

    second commit

--- rumusSegitiga.py
+++ rumusSegitiga.py
@@ -1,18 +1,22 @@
 # FORMULA SEGITIGA BY FARREL
 # 05/10/2024 15:46
 
+def segitiga():
+
 # MENGAMBIL INPUT DATA DARI USER
 
-S = int(input("Masukkan Sisi Segitiga : "))
-A = int(input("Masukkan Alas Segitiga : "))
-T = int(input("Masukkan Tinggi Segitiga : "))
+    S = int(input("Masukkan Sisi Segitiga : "))
+    A = int(input("Masukkan Alas Segitiga : "))
+    T = int(input("Masukkan Tinggi Segitiga : "))
 
 #ALGORITMA
 
-luasS = 0.5*A*T 
-kelilingS = S*3 
+    luasS = lambda A,T : 0.5*A*T 
+    kelilingS = lambda S : S*3 
 
 # OUTPUT/HASIL
 
-print(f'''LUAS SEGITIGA : {luasS}cm
-KELILING SEGITIGA : {kelilingS}cm''')
+    print(f'''LUAS SEGITIGA : {luasS(A,T)}cm
+KELILING SEGITIGA : {kelilingS(S)}cm''')
+    
+segitiga()
