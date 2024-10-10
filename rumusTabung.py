60043e1c36671367e24965a9da255b7e34081357
= <=>
2024-10-10 07:32:13 +0700

    second commit

--- rumusTabung.py
+++ rumusTabung.py
@@ -1,17 +1,21 @@
 # PROGRAM FORMULA RUMUS TABUNG BY FARREL
 # 07/10/2024
 
+def tabung():
+
 # MENGAMBIL DATA DARI INPUT USER
 
-r = int(input("Masukkan jari jari tabung : "))
-t = int(input("Masukkan tinggi tabung : "))
+    r = int(input("Masukkan jari jari tabung : "))
+    t = int(input("Masukkan tinggi tabung : "))
 
 # ALGORITMA
 
-luasT = 3.14*r**2*t
-volumeT= 2*3.14*r*t + 2*3.14*r**2
+    luasT = lambda r,t : 3.14*r**2*t
+    volumeT = lambda r,t : 2*3.14*r*t + 2*3.14*r**2
 
 # HASIL/OUTPUT
 
-print(f'''LUAS TABUNG = {luasT}
-VOLUME TABUNG = {volumeT}''')
+    print(f'''LUAS TABUNG = {luasT(r,t)}
+VOLUME TABUNG = {volumeT(r,t)}''')
+    
+tabung()
