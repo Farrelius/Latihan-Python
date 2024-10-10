fa785e08e45d861adddbe3701efb71b7b331d34a
= <=>
2024-10-10 13:03:49 +0700

    second commit

--- rumusPersegiPanjang.py
+++ rumusPersegiPanjang.py
@@ -1,17 +1,21 @@
 # FORMULA PERSEGI PANJANG BY FARREL
 # 05/10/2024 15:01
 
+def persegi_panjang():
+
 # MENGAMBIL INPUT DATA DARI USER
 
-P = int(input("Masukkan panjang persegi panjang : "))
-L = int(input("Masukkan panjang lebar panjang : "))
+    P = int(input("Masukkan panjang persegi panjang : "))
+    L = int(input("Masukkan panjang lebar panjang : "))
 
 # ALGORITMA
 
-luasPP = P*L
-kelilingPP = 2*(P+L)
+    luasPP = lambda P,L : P*L
+    kelilingPP = lambda P,L : 2*(P+L)
 
 # OUTPUT
 
-print(f'''LUAS PERSEGI PANJANG : {luasPP}cm^2
-KELILING PERSEGI PANJANG : {kelilingPP}cm^2''')
+    print(f'''LUAS PERSEGI PANJANG : {luasPP(P,L)}cm^2
+KELILING PERSEGI PANJANG : {kelilingPP(P,L)}cm^2''')
+    
+persegi_panjang()
