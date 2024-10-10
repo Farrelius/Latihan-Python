9f0cdfcdfbfec5603630746fa146f57e21a0bf69
= <=>
2024-10-10 07:49:16 +0700

    second commit

--- rumusTrapesium.py
+++ rumusTrapesium.py
@@ -1,19 +1,23 @@
 # PROGRAM FORMULA TRAPESIUM BY FARREL
 # 07/10/2024 17:20
 
+def trapesium ():
+
 # MENGAMBIL DATA DARI INPUT USER
 
-a = int(input("Masukkan sisi atas : "))
-b = int(input("Masukkan sisi bawah : "))
-t = int(input("Masukkan tingginya : "))
-s = int(input("Masukkan sisi: "))
+    a = int(input("Masukkan sisi atas : "))
+    b = int(input("Masukkan sisi bawah : "))
+    t = int(input("Masukkan tingginya : "))
+    s = int(input("Masukkan sisi: "))
 
 # ALGORITMA
 
-luasT = 0.5*(a+b)*t
-kelilingt = s*4
+    luasT = lambda a,b,t : 0.5*(a+b)*t
+    kelilingt = lambda s : s*4
 
 # HASIL/OUTPUT
 
-print(f'''LUAS TRAPESIUM = {luasT}
-KELILING TRAPESIUM = {kelilingt}''')
+    print(f'''LUAS TRAPESIUM = {luasT(a,b,t)}
+KELILING TRAPESIUM = {kelilingt(s)}''')
+    
+trapesium()
