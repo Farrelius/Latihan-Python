5891d9b2d39464928f69ea836c7396f53ab1ac04
= <=>
2024-11-08 08:03:31 +0700

    first commit

---
+++ Gaya.py
@@ -0,0 +1,15 @@
+# PROGRAM GAYA BY FARREL
+# 08-11-2024
+
+# MENGAMBIL DATA DARI INPUT USER
+
+m = float(input("Masukkan jarak (m): "))
+a = float(input("Masukkan percepatan: "))
+
+# ALGORITMA
+
+F = lambda m,a : m*a
+
+# OUTPUT/HASIL
+
+print(f"Gayanya adalah adalah : {F(m,a)}")
