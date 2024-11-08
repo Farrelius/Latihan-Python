05f112fec9be24507be8dce32bb7f16d1cda46d8
= <=>
2024-11-08 08:00:28 +0700

    first commit

---
+++ rumusMomentum.py
@@ -0,0 +1,15 @@
+# PROGRAM MOMENTUM BY FARREL
+# 08-11-2024
+
+# MENGAMBIL DATA DARI INPUT USER
+
+m = float(input("Masukkan jarak (m): "))
+v = float(input("Masukkan kecepatan (v): "))
+
+# ALGORITMA
+
+M = lambda m,v : m*v
+
+# OUTPUT/HASIL
+
+print(f"Momentumnya adalah : {M(m,v)}")
