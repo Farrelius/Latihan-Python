bba9e0790a838fdb165f09b14e85f19d5520eb27
= <=>
2024-11-08 08:09:19 +0700

    first commit

---
+++ energiPotensial.py
@@ -0,0 +1,16 @@
+# PROGRAM ENERGI POTENSIAL BY FARREL
+# 04-11-2024 08:06
+
+# MENGAMBIL DATA DARI INPUT USER
+
+m = float(input("Masukkan massa : "))
+g = float(input("Masukkan gravitasi : "))
+h = float(input("Masukkan ketinggian : "))
+
+# ALGORITMA
+
+EP = lambda m,g,h : m*g*h
+
+# OUTPUT/HASIL
+
+print(f"Energi potensialnya adalah : {EP(m,g,h)}")
