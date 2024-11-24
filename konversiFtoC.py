dc061a4071d9277b0eef3a30e240fce69faf6360
= <=>
2024-11-24 17:25:47 +0700

    first commit

---
+++ konversiFtoC.py
@@ -0,0 +1,14 @@
+# KONVERSI FAHRENHEIT KE CELCIUS BY FARREL
+# 24-11-2024 17:19
+
+# MENGAMBIL DATA DARI INPUT USER
+
+f = float(input("Masukkan suhu fahrenheit : "))
+
+# ALGORITMA
+
+c = (f-32)*5/9
+
+# OUTPUT
+
+print("Fahrenheit --> Celcius =", c)
