1f0d32d311e7b61e0c913d107596c798bffb5825
= <=>
2024-11-25 16:20:35 +0700

    first commit

---
+++ konversiKtoF.py
@@ -0,0 +1,10 @@
+# KONVERSI KELVIN KE FAHRENHEIT BY FARREL
+# 25-11-2024 16:06
+
+# MENGAMBIL DATA DARI INPUT USER
+
+k = float(input("Masukkan derajat kelvin : "))
+
+f = (k - 273.15)*9/5 + 32
+
+print("Kelvin to Fahrenheit --> ", f)
