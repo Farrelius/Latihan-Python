6c61528231d98ffb90c65875bd7acadf3c2213f0
= <=>
2024-11-25 16:30:50 +0700

    first commit

---
+++ konversiCtoR.py
@@ -0,0 +1,9 @@
+# KONVERSI CELCIUS KE REAMUR BY FARREL
+# 25-11-2024 16:22
+
+
+c = float(input("Masukkan nilai suhu celcius nya : "))
+
+r = (4/5)*c
+
+print("Celcius --> Reamur ", r)
---
+++ konversiRtoC.py
@@ -0,0 +1,9 @@
+# KONVERSI REAMUR TO CELCIUS BY FARREL
+# 25-11-2024 16:27
+
+
+r = float(input("Masukkan nilai suhu reamur nya : "))
+
+c = (5/4)*r
+
+print("Reamur --> Celcius =  ", c)
