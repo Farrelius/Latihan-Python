3cca76f6300fa4f393a17bf8b0f0f9b1f782177b
= <=>
2024-11-29 18:17:45 +0700

    first commit

---
+++ konversiKarakterToNumber.py
@@ -0,0 +1,56 @@
+char = str(input("Masukkan karakter untuk ke nomor : "))
+
+
+match char:
+    case "a":
+        print(1)
+    case "b":
+        print(2)
+    case "c":
+        print(3)
+    case "d":
+        print(4)
+    case "e":
+        print(5)
+    case "f":
+        print(6)
+    case "g":
+        print(7)
+    case "h":
+        print(8)
+    case "i":
+        print(9)
+    case "j":
+        print(10)
+    case "k":
+        print(11)
+    case "l":
+        print(12)
+    case "m":
+        print(13)
+    case "n":
+        print(14)
+    case "o":
+        print(15)
+    case "p":
+        print(16)
+    case "q":
+        print(17)
+    case "r":
+        print(18)
+    case "s":
+        print(19)
+    case "t":
+        print(20)
+    case "u":
+        print(21)
+    case "v":
+        print(22)
+    case "w":
+        print(23)
+    case "x":
+        print(24)
+    case "y":
+        print(25)
+    case "z":
+        print(26)
