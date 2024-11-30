ed313b8efd538e38fb81483f922df1f76ecb7cf0
= <=>
2024-11-30 14:43:31 +0700

    first commit

---
+++ membalikString.py
@@ -0,0 +1,3 @@
+text = str(input("Masukkan angka sembarang yang ingin di balik : "))
+reversed_text = text[::-1]
+print("String yang dibalik:",reversed_text)
---
+++ menghitungpangkat.py
@@ -0,0 +1,6 @@
+n = float(input("Masukkan nilai yang dipangkatkan : "))
+nP = int(input("Masukkan nilai pangkatkanya : "))
+
+hasil = n**nP
+
+print(hasil)
