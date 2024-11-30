c4eac0ddf10213327dbef07730a11cbae1444ca0
= <=>
2024-12-01 01:31:34 +0700

    first commit

---
+++ kmTOMeter.py
@@ -0,0 +1,5 @@
+km = float(input("Masukkan jarak (km): "))
+
+m = km*1000
+
+print(f"KM --> M : {m}")
---
+++ kmToCm.py
@@ -0,0 +1,5 @@
+km = float(input("Masukkan jarak (km): "))
+
+cm = km*100000
+
+print(f"KM --> CM : {cm}")
