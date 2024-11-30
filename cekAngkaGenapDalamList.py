62b36695aff908b452f6d5549fd854b142cf5900
= <=>
2024-11-30 18:47:26 +0700

    first commit

--- cekAngkaGenapDalamList.py
+++ cekAngkaGenapDalamList.py
@@ -8,6 +8,6 @@ while add_list<=b_list:
     list.append(int(input("Masukkan angka ke dalam list : ")))
     add_list+=1
 
-bilangan_ganjil = sum(1 for i in list if i % 2 == 1)
+bilangan_genap = sum(1 for i in list if i % 2 == 1)
 
-print("Total bilangan ganjil dalam list adalah : ", bilangan_ganjil)
+print("Total bilangan genap dalam list adalah : ", bilangan_genap)
